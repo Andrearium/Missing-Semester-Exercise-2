5778dc14b9045ac92f5effaa7d3de93aa3ee5b08
Andrea Zoitl <strickarium@gmail.com>
2024-02-06 13:39:15 +0100

    updated shell script, removed unused background

--- deploy/pushToMissingSemesterServer.sh
+++ deploy/pushToMissingSemesterServer.sh
@@ -8,12 +8,15 @@ scp background3.jpg missing.sai.jku.at:/home/k00340477/public_html/Ex2
 scp background4.png missing.sai.jku.at:/home/k00340477/public_html/Ex2
 scp BadEval.html missing.sai.jku.at:/home/k00340477/public_html/Ex2
 scp frog.jpg missing.sai.jku.at:/home/k00340477/public_html/Ex2
+scp frog1.png missing.sai.jku.at:/home/k00340477/public_html/Ex2
+scp Game.html missing.sai.jku.at:/home/k00340477/public_html/Ex2
 scp GoodEval.html missing.sai.jku.at:/home/k00340477/public_html/Ex2
 scp meme.jpg missing.sai.jku.at:/home/k00340477/public_html/Ex2
 scp MissingSemesterEvaluation.html missing.sai.jku.at:/home/k00340477/public_html/Ex2/index.html
 scp stylesEval.css missing.sai.jku.at:/home/k00340477/public_html/Ex2
 scp stylesEvalBad.css missing.sai.jku.at:/home/k00340477/public_html/Ex2
 scp stylesEvalGood.css missing.sai.jku.at:/home/k00340477/public_html/Ex2
+scp stylesGame.css missing.sai.jku.at:/home/k00340477/public_html/Ex2
 
 ssh missing.sai.jku.at mdir -p /home/k00340477/public_html/Ex2/EvaluationGame/HTML
 scp -r EvaluationGame/HTML/* missing.sai.jku.at:/home/k00340477/public_html/Ex2/EvaluationGame/HTML
